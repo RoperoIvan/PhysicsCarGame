7fb3ea33b3c97e2e59d55b0407a0319b54f745f0
RoperoIvan <ivan_ropero98@hotmail.com>
2018-12-09 13:07:18 +0100

    first circuit

--- ModuleSceneIntro.cpp
+++ ModuleSceneIntro.cpp
@@ -2,7 +2,7 @@
 #include "Application.h"
 #include "ModuleSceneIntro.h"
 #include "Primitive.h"
-#include "PhysBody3D.h"
+#include "PhysBody3D.h" 
 
 ModuleSceneIntro::ModuleSceneIntro(Application* app, bool start_enabled) : Module(app, start_enabled)
 {
@@ -16,6 +16,12 @@ bool ModuleSceneIntro::Start()
 {
 	LOG("Loading Intro assets");
 	bool ret = true;
+
+	for (int j = 0; j < 10; j++) {
+		for (int i = 0; i < 4; i++) {
+			CreateFloor(vec3(30, 1, 30), 30 * i, 30 * j);
+		}
+	}
 
 	App->camera->Move(vec3(1.0f, 1.0f, 0.0f));
 	App->camera->LookAt(vec3(0, 0, 0));
@@ -35,13 +41,34 @@ bool ModuleSceneIntro::CleanUp()
 update_status ModuleSceneIntro::Update(float dt)
 {
 	Plane p(0, 1, 0, 0);
+	p.color = Yellow;
 	p.axis = true;
 	p.Render();
+
+	if (pb_cubes.Count() != 0 && s_cubes.Count() != 0 && s_cubes.Count() == pb_cubes.Count()) {
+		for (int i = 0; i < s_cubes.Count(); i++) {
+			pb_cubes[i]->GetTransform(&s_cubes[i].transform);
+			s_cubes[i].Render();
+		}
+
+	}
 
 	return UPDATE_CONTINUE;
 }
 
 void ModuleSceneIntro::OnCollision(PhysBody3D* body1, PhysBody3D* body2)
 {
+}
+void ModuleSceneIntro::CreateFloor(vec3 scale, int posX, int posZ)
+{
+	Cube cubes;
+	PhysBody3D* pb_cube;
+
+	    cubes.Scale(scale.x, scale.y, scale.z);
+		s_cubes.PushBack(cubes);
+		pb_cube = App->physics->AddBody(cubes, 0);
+		pb_cube->SetPos(posX, 0, posZ);
+		pb_cubes.PushBack(pb_cube);
+	
 }
 
