11fc17cff6f7a1b10140302f4c6301dfec4ffe5e
RoperoIvan <ivan_ropero98@hotmail.com>
2018-12-19 14:19:02 +0100

    Memory leak fixed
    The limits now are created after the circuit creation in the start method

--- ModuleSceneIntro.cpp
+++ ModuleSceneIntro.cpp
@@ -35,7 +35,14 @@ bool ModuleSceneIntro::Start()
 	};
 	//load circuit1, only for 7-column circuits
 	LoadCircuit(circuit, circuit1);
-
+	if (pb_limits.Count() != 0 && s_limits.Count() != 0 && s_limits.Count() == pb_limits.Count())
+	{
+	for (int i = 0; i < s_limits.Count(); i++) {
+	/*pb_limits[i]->GetTransform(&s_limits[i].transform);*/
+	pb_limits[i]->SetAsSensor(true);
+	pb_limits[i]->collision_listeners.add(this);
+	}
+	}
 	sensor_victory.color = Yellow;
 	sensor_victory.Size(30, 1, 15);
 	pb_victory = App->physics->AddBody(sensor_victory, 0);
@@ -46,7 +53,7 @@ bool ModuleSceneIntro::Start()
 
 	App->camera->Move(vec3(1.0f, 1.0f, 0.0f));
 	App->camera->LookAt(vec3(0, 0, 0));
-	float size = 2.0f;
+	/*float size = 2.0f;
 	for(int i = 0; i < 5; i++)
 	{
 		chain[i].radius = size;
@@ -57,7 +64,7 @@ bool ModuleSceneIntro::Start()
 			App->physics->AddConstraintHinge(*pb_chain[i], *pb_chain[i - 1], vec3(-2*size, 0, 0), vec3(0, 0, 0), vec3(0, 1, 0), vec3(0, 1, 0));
 		}
 		
-	}
+	}*/
 
 	for (int i = 0; i < 70; ++i)
 	{
@@ -228,25 +235,16 @@ void ModuleSceneIntro::Painting()
 	{
 		for (int i = 0; i < s_cubes.Count(); i++) {
 			pb_cubes[i]->GetTransform(&s_cubes[i].transform);
-			if(pb_cubes[i]->paiting == true)
+			/*if(pb_cubes[i]->paiting == true)*/
 				s_cubes[i].Render();
 		}
 
 	}
-
-	if (pb_limits.Count() != 0 && s_limits.Count() != 0 && s_limits.Count() == pb_limits.Count()) 
-	{
-		for (int i = 0; i < s_limits.Count(); i++) {
-			pb_limits[i]->GetTransform(&s_limits[i].transform);
-			pb_limits[i]->SetAsSensor(true);
-			pb_limits[i]->collision_listeners.add(this);
-		}
-	}
-	for (int i = 0; i < 5; i++)
+	/*for (int i = 0; i < 5; i++)
 	{
 		pb_chain[i]->GetTransform(&(chain[i].transform));
 		chain[i].Render();
-	}
+	}*/
 }
 
 int ModuleSceneIntro::Size(int * vec)
